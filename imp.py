8520daf4cbcdc6c39064fc4a8b0c06bc88791494
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2019-05-14 17:29:12 -0500

    Create imp.py

---
+++ imp.py
@@ -0,0 +1,16 @@
+import sys
+
+from imp_lexer import *
+
+def usage():
+    sys.stderr.write('Usage: imp filename\n')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    filename = sys.argv[1]
+    text = open(filename).read()
+    tokens = imp_lex(text)
+    for t in tokens:
+        print t
